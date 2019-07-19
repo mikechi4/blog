6d3f1f6dbeecf4f82477f67133e44f159949f0e4
Mike Chi <mike.chi4@gmail.com>
2019-07-19 07:56:58 -0400

    adding new component to display user name

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
+import UserHeader from './UserHeader';
 class PostList extends React.Component {
     componentDidMount() {
         this.props.fetchPosts()
@@ -18,6 +19,7 @@ class PostList extends React.Component {
                             <h2>{post.title}</h2>
                             <p>{post.body}</p>
                         </div>
+                        <UserHeader userId={post.userId} />
                     </div>
                 </div>
             );
